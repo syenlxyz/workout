84ec8dc785e5a3bdeb4457ef62ff466bb46e8f26
SyenLxyz <syenlxyz@gmail.com>
2022-08-02 15:05:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var id = 'push'
+var id = 'pull'
 
 var tab = document.getElementById(id);
 tab.classList.add('active');
 
 var tabPane = document.getElementById(id + '-link');
 tabPane.classList.add('active');
+
+// 2022-08-01: push
