312158423c5e721aa0ce48e7b126207d389d537b
SyenLxyz <syenlxyz@gmail.com>
2022-08-03 14:05:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var id = 'pull'
+var id = 'legs'
 
 var tab = document.getElementById(id);
 tab.classList.add('active');
@@ -7,3 +7,4 @@ var tabPane = document.getElementById(id + '-link');
 tabPane.classList.add('active');
 
 // 2022-08-01: push
+// 2022-08-02: pull
