79f4f2e1dea9ce0fec188e0c6fd0cc953c9d2b3f
SyenLxyz <syenlxyz@gmail.com>
2022-08-15 18:54:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-var id = 'day1'
+var dow = {
+  0: 'day1',
+  1: 'day1',
+  2: 'day2',
+  3: 'day3',
+  4: 'day4',
+  5: 'day5',
+  6: 'day6'
+};
+
+var today = new Date();
+var id = dow[today.getDay()];
 
 var tab = document.getElementById(id);
 tab.classList.add('active');
@@ -12,5 +23,5 @@ tabPane.classList.add('active');
 // 2022-08-04: push
 // 2022-08-05: pull
 // 2022-08-13: legs
-
-// 2022-08-15: push (tbc)
+// 2022-08-15: push
+// 2022-08-16: pull (tbc)
