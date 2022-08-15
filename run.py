8a1f038a18586719471bfe6bebac4f52c704a4e9
SyenLxyz <syenlxyz@gmail.com>
2022-08-15 18:29:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,105 @@
 # Load python packages
+from alive_progress import alive_it, config_handler
 from bs4 import BeautifulSoup
+from docx import Document
 from pathlib import Path
 from tabulate import tabulate
 import json
+import multiprocessing as mp
+import pandas as pd
+import re
+import requests
+
+# Define user agent
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+ 
+config_handler.set_global(
+    length=75,
+    spinner='classic',
+    bar='classic2',
+    force_tty=True,
+    dual_line=True
+)
+
+def create_json():
+    file_path = Path.cwd() / 'workout.docx'
+    document = Document(file_path)
+
+    paragraphs = document.paragraphs
+    titles = [x.text for x in paragraphs if x.text.strip()]
+
+    tables = document.tables
+    keys = ['day1', 'day2', 'day3', 'day4', 'day5', 'day6']
+
+    data = {}
+    for index, table in enumerate(tables):
+        title = titles[index]
+        print(title)
+
+        df = create_df(table)
+        df['Image'] = get_images(df)
+
+        key = keys[index]
+        data[key] = df.to_dict('records')
+
+    json_path = Path.cwd() / 'data.json'
+    with open(json_path, 'w') as file:
+        json.dump(data, file)
+
+def create_df(table):
+    num_row = len(table.rows)
+    num_col = len(table.rows[0].cells)
+    for i in range(num_row):
+        row = table.rows[i]
+        if i == 0:
+            header = [x.text.strip() for x in row.cells]
+            data = {x:[] for x in header}
+        else:
+            for j in range(num_col):
+                key = header[j]
+                item = row.cells[j].text.strip()
+                data[key].append(item)
+    df = pd.DataFrame(data)
+    return df
+
+def get_images(df):
+    links = list(df['Link'])
+    processes = mp.cpu_count() - 1
+    with mp.Pool(processes) as pool:
+        results = alive_it(
+            pool.imap(get_image, links),
+            len(links)
+        )
+        images = []
+        for index, result in enumerate(results):
+            results.text(f'Download: {links[index]}')
+            images.append(result)
+        return images
+
+def get_image(url):
+    filename = url.split('/')[-2] + '.svg'
+    url = get_link(url)
+    if url:
+        headers = {
+            'User-Agent': USER_AGENT
+        }
+        response = requests.get(url, headers=headers)
+        image = response.content
+        image_path = Path('image') / filename
+        with open(image_path, 'wb') as file:
+            file.write(image)
+        return image_path.as_posix()
+
+def get_link(url):
+    headers = {
+        'User-Agent': USER_AGENT
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img = soup.find('img', {'class': 'exImg'})
+    if img:
+        link = img['data-url_male']
+        return link
 
 # Create html file
 def create_html():
@@ -47,7 +144,7 @@ def create_table(array):
         row = [
             item['No'],
             create_link(item['Exercise'], item['Link']),
-            create_img(item['Image'], item['Link']),
+            create_img(item['Image']),
             item['Body Part']
         ]
         table.append(row)
@@ -65,13 +162,14 @@ def create_link(name, url):
     return str(a)
 
 # Create <img> tag element
-def create_img(src, url):
+def create_img(url):
     soup = BeautifulSoup(features='html.parser')
     img = soup.new_tag('img')
-    img['src'] = src
+    img['src'] = url
     img['alt'] = url.split('/')[-1]
     return str(img)
 
 # Run main program
 if __name__ == '__main__':
+    create_json()
     create_html()
